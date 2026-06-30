c190a2746a414b9622b0c30a7cfcb153860f437e
sapna-0514 <sapnakumari68146@gmail.com>
2026-06-30 20:08:10 +0530

    Integrate Streamlit for mbox file processing
    Refactor code to use Streamlit for file upload and display results.

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
-from PIL import Image
-import numpy as np
+
+import streamlit as st
 import mailbox
 from collections import Counter
 import string
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
-from wordcloud import STOPWORDS
 
-mbox = mailbox.mbox('sample.mbox')
+uploaded_file = st.file_uploader("Upload an .mbox file", type="mbox")
 
-all_words = []
+if uploaded_file is not None:
+    mbox = mailbox.mbox(uploaded_file.name)
+    all_words = []
 
-for message in mbox:
-    body = message.get_payload()
-    if body:  
-        body = body.lower()  
-        body = body.translate(str.maketrans('', '', string.punctuation))  
-        words = body.split()  
-        all_words.extend(words)
+    for message in mbox:
+        body = message.get_payload()
+        if body:
+            body = body.lower()
+            body = body.translate(str.maketrans('', '', string.punctuation))
+            words = body.split()
+            all_words.extend(words)
 
+    count = Counter(all_words)
+    st.write("Top 10 words:", count.most_common(10))
 
-count = Counter(all_words)
-print("Top 10 words:", count.most_common(10))
+    wc = WordCloud(width=800, height=400, background_color='white')
+    wc.generate_from_frequencies(count)
 
-
-wc = WordCloud(width=800, height=400, background_color='white', colormap='rainbow')
-wc=WordCloud(stopwords=STOPWORDS,background_color='white')
-
-wc.generate_from_frequencies(count)
-
-plt.figure(figsize=(10, 5))
-plt.imshow(wc, interpolation='bilinear')
-plt.axis('off')
-plt.show()
+    st.image(wc.to_array(), use_column_width=True)
